fd4c20623f50cc2b7bd729f8fa8f839c103f4863
Nancy Lee <107009879+nancylee1@users.noreply.github.com>
2022-07-05 11:14:05 -0400

    initial commit

---
+++ Methods_Exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿// Ask the user “What is your name?”
+// Store the input
+
+Console.WriteLine("What is your name?"); // output
+var name = Console.ReadLine(); // input
+
+// Record user input, for each of these. Combine all answers for a fun, short story.
+//EXAMPLES:
+
+//Name: Michael
+
+//Favorite Color: Blue
+
+//Favorite Animal: Walrus
+
+//Favorite Band: The Beatles
+
+//Extra points - turn the answers into a fun short story!
+//Save, commit, and push to Github
+
+Console.WriteLine("What is your favorite color?"); 
+var color = Console.ReadLine();
+
+Console.WriteLine("What is your favorite animal?");
+var animal = Console.ReadLine();
+
+Console.WriteLine("What is your favorite band?");
+var band = Console.ReadLine();
+
+
+Console.WriteLine($"Hello, my name is {name} and my favorite animal is a {animal}. I want to dye my {animal} {color} and play {band} on the Sonos all day."); 
